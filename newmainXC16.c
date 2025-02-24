8742f4ac4ccc120433f01bf38be55ab1bf126e8a
Milad Rabiei <61626774+milwd@users.noreply.github.com>
2025-02-24 10:17:39 +0100

    Update newmainXC16.c

--- newmainXC16.c
+++ newmainXC16.c
@@ -1,7 +1,6 @@
-#include "xc.h"
+#include <p33EP512MU810.h>
 
-#define FCY 16000000UL
-#include <libpic30.h>
+#include "xc.h"
 
 int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
@@ -13,28 +12,28 @@ int main(void) {
 //    while{
 //    }
     
-    //assignment part 2
-    TRISEbits.TRISE8    = 1; // set as input
-    uint8_t t2_status;
     
+    TRISEbits.TRISE8    = 1; // set as input
+    uint8_t t2_status, t2_status_prev;
+    
+    //assignment part 2
 //    while (1){
 //        t2_status = !PORTEbits.RE8;
 //        if (t2_status){
 //            LATAbits.LATA0 = 1;  // turn on led 1
-//            __delay_ms(500);
 //        } else {
 //            LATAbits.LATA0 = 0;  // turn off led 1
 //        }
 //    }
     
-    // assignment part 3
-//    while (1){
-//        t2_status = !PORTEbits.RE8;
-//        if (t2_status){
-//             LATAbits.LATA0 ^= 1;           
-//        }
-//        __delay_ms(1000);
-//    }
+    // assignment part 3 -- rising edge detector
+    while (1){
+        t2_status = PORTEbits.RE8;
+        if (t2_status && !t2_status_prev){
+            LATAbits.LATA0 ^= 1;
+        }
+        t2_status_prev = t2_status;
+    }
     
     return 0;
 }
